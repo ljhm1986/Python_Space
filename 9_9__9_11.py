6393a61f9dccf0a2254a2c4797e3735a0b9460d5
ljhm1986 <korea__2005@hanmail.net>
2019-09-10 18:29:42 +0900

    9/10

--- 9_9__9_11.py
+++ 9_9__9_11.py
@@ -456,3 +456,539 @@ while True:
     print(data, end = '')
 f.close()
 
+###############################################################
+#9/10#
+
+import os
+#해당 파일이 존재하는지 확인하자 
+os.path.exists("C:\\WorkSpace\\Python_Space\\data\\test.txt")
+#True
+
+#읽기 전용으로 불러들이자 
+file = open('C:\\WorkSpace\\Python_Space\\data\\test.txt','r')
+while True:
+    data = file.readline()
+    if not data:
+        break
+    print(data, end = '')
+file.close()
+
+file = open('C:\\WorkSpace\\Python_Space\\data\\test.txt','r')
+data = file.readlines()
+data
+print(data)
+file.close()
+data
+
+#file을 open, close를 동시에 하면 어떨까?
+#pl/sql을 할때 cousor(sql문 실행 메모리 영역) 를 활용할때 처럼
+#(커서선언 -> 커서 open -> fetch -> 커서 close)
+#이때 fetch 문을 for 문으로 바꾸어서 동시에 했다.
+#with문 : 파일객체를 자동으로 열고 닫아주는 문 
+
+with open('C:\\WorkSpace\\Python_Space\\data\\test.txt','w') as file:
+    for i in range(1,11):
+        txt = '%d 오늘 하루도 행복하자\n'%i
+        file.write(txt)
+        
+txt = ['야!! 추석이다','오늘 하루도 신난다. 크아아악']
+
+with open('C:\\WorkSpace\\Python_Space\\data\\test1.txt','w') as file:
+    for i in txt:
+        file.write(i+'\n')
+
+with open('C:\\WorkSpace\\Python_Space\\data\\test1.txt','r') as file:
+    data = file.readlines()
+    for i in data:
+        print(i, end='')
+
+##################################        
+#이제 sql developer 로 가서 익스포트를 하자
+#oracle sql developer - 도구 - 데이터베이스 익스포트
+
+#DDL익스포트는 안 함 
+#파일 형식 - csv 형식
+#헤더 - column name 까지 포함 
+#행 터미네이터 - 환경 기본값  
+#둘러싸기 - 없음 (문자는 "가 둘러싼 형태로 있는데 없게 하자)
+#인코딩 - MS949
+
+#토글 - 테이블 만 
+
+#조회 - 익스포트할 테이블만
+###################################
+#csv 파일을 불러보자 
+import csv
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+emp_csv#<_csv.reader at 0x791c388>
+
+for i in emp_csv:
+    print(i[2],i[7])
+  
+#내용물을 보고 싶으면 다음과 같이 list로 옮겨놓자
+emp_see = []
+for i in emp_csv:
+    emp_see.append(i)
+emp_see
+emp_see[0]
+
+#column중에 하나를 key값으로 하고 dictionary에 옮겨놓자 
+#key는 column의 값이 unique한 것으로 고르자  
+emp_see2 = {}
+for i in emp_csv:
+    emp_see2[i[0]] = i[1:]
+emp_see2['100']
+emp_see2.keys()
+emp_see2.values()
+emp_see2.items()
+
+file.close()
+
+#column이름은 보기 싫으면
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+#첫번째 줄을 읽으면서 pop한거처럼 된다.
+next(emp_csv)
+next(emp_csv)
+
+for i in emp_csv:
+    print(i[2],i[7])
+    
+file.close()
+
+#[문제 63] emp.csv 파일에서 last_name, last_name 길이를 출력해주세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+#next()를 사용해서 last_name이 몇번째 column인지 보자 
+next(emp_csv)#3번째이다
+
+for i in emp_csv:
+    print(i[2], len(i[2]))
+#뭐지 한 번하고 또 하면 안 나오네 
+    
+file.close()
+
+#[문제 64] employee_id, last_name, salary * 12 값을 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#1번째, 3번째, 8번째
+for i in emp_csv:
+    print(i[0], i[2], float(i[7])*12)
+file.close()
+
+#[문제 65] last_name, commission_pct 를 출력하는데 
+#commission_pct값이 ''이면 0으로 출력해주세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)
+
+for i in emp_csv:   
+    print(i[2], ifnull(i[8],0))
+#값이 없으면 아무것도 나오지 않는다.
+    
+def ifnull(arg1, arg2):
+    if arg1 == '':
+        return arg2
+    return arg1
+
+file.close()
+#
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)
+
+for i in emp_csv:   
+    print(i[2], 0 if i[8] == '' else i[8])
+    
+
+file.close()
+
+
+#[문제 66번] last_name은 대문자, job_id는 소문자로 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번쨰, 7번째
+
+for i in emp_csv:   
+    print(i[2].upper(), i[6].lower())
+    
+file.close()
+
+#[문제 67번] last_name은 첫글자만 추출해서 소문자로 출력해주세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번째
+
+for i in emp_csv:   
+    print(i[2][0].lower())
+    
+file.close()
+
+
+#[문제 68번] last_name의 두번쨰부터 마지막까지 추출해서 대문자로 출력해주세
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번째
+
+for i in emp_csv:   
+    print(i[2][1:].upper())
+    
+file.close()
+
+#[문제 69] 이름을 입력하면 첫글자는 대문자, 나머지는 소문자를 
+#추력하는 initcap 함수를 이용해서 이름을 출럭하기
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번째
+
+def initcap(arg):
+    return arg[0].upper() + arg[1:].lower()
+
+for i in emp_csv:   
+    print(initcap(i[2]))
+    
+file.close()
+
+#[문제 70] 이름을 입력하면 제일 뒤에 있는 철자는 대문자, 
+#앞의 문자는 소문자를 출력하는 tailcap함수를 생성하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번째
+
+def tailcap(arg):
+    return arg[0:-1].lower() + arg[-1].upper()
+
+for i in emp_csv:   
+    print(tailcap(i[2]))
+    
+file.close()
+
+#[문제 71] last_name, salary를 출력하는데 salary값 중에
+#0대신 *를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번째, 8번째
+
+for i in emp_csv:   
+    print(i[2], i[7].replace('0','*'))
+    
+file.close()
+
+#[문제 72] last_name, salary*12 + commission_pct값을 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3번째, 8번째, 9번째
+
+def ifnull(arg1, arg2):
+    if arg1 == '':
+        return arg2
+    return arg1
+
+for i in emp_csv:   
+    print(i[2], float(i[7])*12+float(ifnull(i[8],0)))
+    
+file.close()
+
+#[문제 73] last_name, hire_date 는 요일(한글)을 출력하세요.
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3, 6
+
+import datetime
+weekdays = ['월요일','화요일','수요일','목요일','금요일','토요일','일요일']
+week_num = datetime.datetime.strptime(i[5],'%Y-%m-%d').weekday()
+
+for i in emp_csv:
+    print(i[2],weekdays[week_num])
+
+file.close()
+
+#[문제 74] last_name, 근무한 일수를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)
+
+import datetime
+end = datetime.datetime.now()
+hire_day = datetime.datetime.strptime(i[5],'%Y-%m-%d')
+
+
+for i in emp_csv:
+    print(i[2],(end-hire_day).days)
+
+file.close()
+
+#다음과 같을때
+end = datetime.datetime.now()
+hire_day = datetime.datetime.strptime('2019-01-01','%Y-%m-%d')
+(end-hire_day).days#252
+
+datetime.date.today() - hire_day#이건 안 된다. 모양이 맞아야 한다.
+datetime.date.today() - hire_day.date()#이렇게 해야 된다.
+(datetime.date.today() - hire_day.date()).days
+
+#[문제 75] 오늘부터 이번달 말일까지 몇일 남았는지 출력하세요
+import datetime
+import calendar
+
+today = datetime.datetime.now()
+last_day = calendar.monthrange(2019,9)[1]
+remainderDay = (datetime.datetime(2019,9,last_day) - today).days
+print(remainderDay)
+
+##
+from datetime import date
+from calendar import monthrange
+
+monthrange(2019,9)[1] - date.today().day
+
+#[문제 76] 사원번호가 100번 사원의 last_name, salary를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3, 8
+
+for i in emp_csv:
+    if int(i[0]) == 100:
+        print(i[2],i[7])
+
+file.close()
+
+#[문제 77] 급여가 10000 이상인 사원들의 last_name, salayr를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3, 8
+
+for i in emp_csv:
+    if int(i[7]) >= 10000:
+        print(i[2], i[7])
+
+file.close()
+
+#[문제 78] 2001-01-13일에 입사한 사원의 last_name, hire_date를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3, 6
+
+for i in emp_csv:
+    if i[5] == '2001-01-13':
+        print(i[2],i[5])
+
+file.close()
+
+#time library를 사용한다면
+import time  
+
+for i in emp_csv:
+    if (time.strptime(i[5],'%Y-%m-%d') == 
+        time.strptime('2001-01-13','%Y-%m-%d')):
+        print(i[2],i[5])
+
+#[문제 79] 2002년도에 입사한 사원들의 last_name, hire_date를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)
+
+for i in emp_csv:
+    if i[5][0:4] == '2002':
+        print(i[2],i[5])
+        
+file.close()
+
+#time library를 사용한다면
+import time  
+
+for i in emp_csv:
+    if (time.strptime(i[5],'%Y-%m-%d').tm_year == 
+        time.strptime('2002','%Y').tm_year):
+        print(i[2],i[5])
+        
+#[문제 80] 9월에 입사한 사원들의 last_name, hire_date를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)      
+
+for i in emp_csv:
+    if i[5][5:7] == '09':
+        print(i[2],i[5])
+
+file.close()
+
+#time library를 사용한다면
+import time  
+
+for i in emp_csv:
+    if(time.strptime(i[5],'%Y-%m-%d').tm_mon == 
+       time.strptime('09','%m').tm_mon):
+        print(i[2],i[5])
+        
+#[문제 81] job이 ST_CLERK이고 급여가 3000 이상인 사원들의 
+#last_name, job_id, salary를 출력하세요.
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3,7,8
+    
+all_num = 0
+part_num = 0
+
+for i in emp_csv:
+    all_num += 1
+    if (i[6] == 'ST_CLERK') & (int(i[7]) >= 3000):
+        part_num += 1
+        print(i[2],i[6],int(i[7]))
+print('all_num : {}, part_num : {}'.format(all_num, part_num))
+print('비율 {}%'.format(round((part_num*100)/all_num)))
+
+file.close()
+        
+#[문제 82] 급여가 2500~3500 사이인 사원들의 
+#last_name, salary를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3,7,8
+   
+all_num = 0
+part_num = 0
+ 
+for i in emp_csv:
+    all_num += 1
+    if (int(i[7]) >= 2500) & (int(i[7]) <= 3500):
+        part_num += 1
+        print(i[2],int(i[7]))
+print('all_num : {}, part_num : {}'.format(all_num, part_num))
+print('비율 {}%'.format(round((part_num*100)/all_num)))
+
+file.close() 
+   
+#[문제 83] job이 AD_VP, AD_PRES인 사원들의 
+#last_name, job_id를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3,7,8
+
+all_num = 0
+part_num = 0
+    
+for i in emp_csv:
+    all_num += 1
+    if i[6] in ['AD_VP', 'AD_PRES']:
+        part_num += 1
+        print(i[2],i[6])
+print('all_num : {}, part_num : {}'.format(all_num, part_num))
+print('비율 {}%'.format(round((part_num*100)/all_num)))
+
+file.close()     
+
+#[문제 84] job이 AD_VP, AD_PRES가 아닌 사원들의 
+#last_name, job_id를 출력하세요
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+next(emp_csv)#3,7,8
+
+all_num = 0
+part_num = 0
+    
+for i in emp_csv:
+    all_num += 1
+    #in 앞에 not만 추가하면 된다.
+    if i[6] not in ['AD_VP', 'AD_PRES']:
+        part_num += 1
+        print(i[2],i[6])
+print('all_num : {}, part_num : {}'.format(all_num, part_num))
+print('비율 {}%'.format(round((part_num*100)/all_num)))
+
+file.close()
+
+#[문제 85] 커미션이 null 인 사원의 이름, 급여, 커미션을 출력하세요
+with open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r') as file:
+    emp_csv = csv.reader(file)
+    print(next(emp_csv))
+    
+    for i in emp_csv:
+        if i[8] == '':
+            print(i[2],i[7],i[8])
+
+#[문제 86] 커미션이 null 이 아닌 사원들의 이름, 급여, 커미션을 출력하세요
+with open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r') as file:
+    emp_csv = csv.reader(file)
+    print(next(emp_csv))
+    
+    for i in emp_csv:
+        if i[8] != '':
+            print(i[2],i[7],i[8])            
+            
+
+#[문제 87] last_name의 첫글자가 S로 시작하는 사원들의 이름과 급여를 출력하세요
+with open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r') as file:
+    emp_csv = csv.reader(file)
+    print(next(emp_csv))
+    
+    for i in emp_csv:
+        if i[2].startswith('S'):
+            print(i[2],i[7])              
+
+#[문제 88] last_name의 두번째 철자가 i인 사원들의 이름과 월급을 출력하세요
+with open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r') as file:
+    emp_csv = csv.reader(file)
+    print(next(emp_csv))
+    
+    for i in emp_csv:
+        if i[2][1]=='i':
+            print(i[2],i[7])
+
+#[문제 89] 50번 부서 사원들이 last_name, salary를 출력하는데 
+#last_name을 오름차순으로 정렬하세
+with open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r') as file:
+    emp_csv = csv.reader(file)
+    print(next(emp_csv))
+    
+    dic = {}
+    
+    for i in emp_csv:
+        if i[10] == '50':
+            dic[i[2]] = i[7]
+    for key, value in sorted(dic.items()):
+        print(key, value)
+    
+    #salary 기준으로 정렬하려면 
+    import operator
+    for key, value in sorted(dic.items(),key = operator.itemgetter(1)):
+        print(key, value)
+
+##dictionary가 아니라 list를 이용해서 풀어보면 
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+print(next(emp_csv))
+emp_50 = []
+
+for i in emp_csv:
+    if i[-1] == '50':
+        emp_50.append(i)
+    
+file.close()
+emp_50#중첩 list모양이 된다.
+
+import operator
+emp_50_sorted = sorted(emp_50, key = operator.itemgetter(7))
+
+for i in emp_50_sorted:
+    print(i[2],i[7])
+
+#또는 처음에 2개의 column만 골라내서 emp_50을 만들자 
+file = open('C:\\WorkSpace\\Python_Space\\data\\emp.csv','r')
+emp_csv = csv.reader(file)
+print(next(emp_csv))
+emp_50 = []
+
+for i in emp_csv:
+    if i[-1] == '50':
+        emp_50.append([i[2],i[7]])
+    
+file.close()
+emp_50#중첩 list모양이 된다.
+
+import operator
+emp_50_sorted = sorted(emp_50, key = operator.itemgetter(1))
+
+for i in emp_50_sorted:
+    print(i[0],i[1])
