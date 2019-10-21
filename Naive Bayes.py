1b93d5f7207a019f6053074ad704ba087c993a77
ljhm_1986 <korea__2005@hanmail.net>
2019-10-21 20:47:12 +0900

    naive bayes

---
+++ Naive Bayes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 09:42:39 2019
+
+@author: stu12
+"""
+
+import pandas as pd
+
+mail = pd.read_csv("C:\\WorkSpace\\Python_Space\\data\\mail.csv",
+                   encoding = 'cp949')
